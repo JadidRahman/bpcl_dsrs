67d84cd248a0435dcbe2380716812ed366e1abf9
JadidRahman <84655534+JadidRahman@users.noreply.github.com>
2026-06-02 12:40:06 +0600

    Update report_mail.php

--- config/report_mail.php
+++ config/report_mail.php
@@ -5,11 +5,11 @@
 
 define('REPORT_SMTP_HOST', 'smtp.gmail.com');
 define('REPORT_SMTP_PORT', 587);
-define('REPORT_SMTP_USER', 'reportbpcl@gmail.com');
+define('REPORT_SMTP_USER', 'your_email@example.com');
 
 // Put your NEW app password here (after regenerating)
-define('REPORT_SMTP_PASS', 'drua aaki zhyj nbzd');
+define('REPORT_SMTP_PASS', 'change_this_random_key');
 
 // Sender info
-define('REPORT_MAIL_FROM', 'reportbpcl@gmail.com');
+define('REPORT_MAIL_FROM', 'your_email@example.com');
 define('REPORT_MAIL_FROM_NAME', 'Report- Bangladesh Psychiatric Care Ltd.');
